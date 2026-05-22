b35e7655afa745c351f858e2fb91ef0dfbea0846
prasert <prasert.pom@bbl.co.th>
2026-05-22 13:19:03 +0700

    grtgtrgtr

--- blocks/adventure-cf/adventure-cf.js
+++ blocks/adventure-cf/adventure-cf.js
@@ -142,8 +142,3 @@ export default async function decorate(block) {
     showError(block, "Failed to load content fragment");
   }
 }
-
-// eslint-disable-next-line no-console
-// eslint-disable-next-line no-console
-// eslint-disable-next-line no-console
-// eslint-disable-next-line no-console
